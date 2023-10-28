b68a7a6db2cff1048e62ce2a5a5d43d5acb8dcae
Christopher Jackson <jacksonchristopher0924@gmail.com>
2023-10-28 18:30:55 -0500

    correcting for loop

--- DNSServer.py
+++ DNSServer.py
@@ -125,8 +125,8 @@ def run_dns_server():
                 rdata_list = []
 
                 if qtype == dns.rdatatype.MX:
-                    for dns.rdatatype.MX in answer_data:
-                        rdata_list.append(MX(dns.rdataclass.IN, dns.rdatatype.MX, addr))
+                    for (pref, server) in answer_data:
+                        rdata_list.append(MX(dns.rdataclass.IN, dns.rdatatype.MX, pref, server))
                 elif qtype == dns.rdatatype.SOA:
                     (dns.rdatatype.A, dns.rdatatype.AA, dns.rdatatype.MX, dns.rdatatype.CNAME, dns.rdatatype.NS,
                      dns.rdatatype.TXT,
